0db52b73aaae4e65d65c2bb2e4ca34df3b5e2373
dvberkel <daan.vanberkel@nedap.com>
2013-03-04 15:38:26 +0100

    respond to requirements analysts

--- js/setup.js
+++ js/setup.js
@@ -17,5 +17,27 @@
 		return name;
 	    }
 	}).decktoc();
+
+	new Tangle (document.getElementById("recipe"), {
+	    initialize: function () {
+		this.people = 4;
+		this.butterPerPerson = 50;
+		this.sugarPerPerson = 75;
+		this.vanillaPerPerson = 4;
+		this.eggsPerPerson = 1;
+		this.flourPerPerson = 45;
+		this.cocoaPerPerson = 17;
+		this.chipsPerPerson = 45;
+	    },
+	    update: function () {
+		this.butter = this.people * this.butterPerPerson;
+		this.sugar = this.people * this.sugarPerPerson;
+		this.vanilla = this.people * this.vanillaPerPerson;
+		this.eggs = this.people * this.eggsPerPerson;
+		this.flour = this.people * this.flourPerPerson;
+		this.cocoa = this.people * this.cocoaPerPerson;
+		this.chips = this.people * this.chipsPerPerson;
+	    }
+	});
     })
 })(jQuery);
